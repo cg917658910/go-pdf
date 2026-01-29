3b07a790febc20318cfff9b79fd2ce99e27ffa98
cg917658910 <917658910@qq.com>
2026-01-29 10:46:03 +0800

    js: avoid alerts and skip OCG toggling when unsupported

--- internal/engine/js.go
+++ internal/engine/js.go
@@ -9,26 +9,7 @@ import (
 )
 
 func injectJS(ctx *model.Context, start, end time.Time) {
-	js := fmt.Sprintf(`(function(){
-try{
- if(typeof this.getField!=="function")throw 1;
- if(!this.getField("tag_probe"))throw 2;
- if(typeof this.setOCGState!=="function")throw 3;
-
- var now=new Date();
- var start=new Date("%s");
- var end=new Date("%s");
-
- this.setOCGState({cState:["OFF"],oCGs:["OCG_Fallback"]});
-
- if(now>=start&&now<=end){
-  this.setOCGState({cState:["ON"],oCGs:["OCG_Normal"]});
- }else{
-  this.setOCGState({cState:["ON"],oCGs:["OCG_Expired"]});
- }
-}catch(e){
- this.setOCGState({cState:["ON"],oCGs:["OCG_Fallback"]});
-}})();`,
+	js := fmt.Sprintf(`(function(){\ntry{\n var now = new Date();\n // check for form/getField and setOCGState support without alerting\n var hasGetField = (typeof this.getField === "function");\n var hasSetOCG = (typeof this.setOCGState === "function");\n var hasTag = false;\n try{ hasTag = !!this.getField("tag_probe"); }catch(e){ hasTag = false; }\n // if tag or setOCG not supported, skip toggling\n if(!hasGetField || !hasTag || !hasSetOCG){ return; }\n\n var start=new Date("%s");\n var end=new Date("%s");\n\n try{\n  this.setOCGState({cState:["OFF"],oCGs:["OCG_Fallback"]});\n  if(now>=start&&now<=end){\n   this.setOCGState({cState:["ON"],oCGs:["OCG_Normal"]});\n  }else{\n   this.setOCGState({cState:["ON"],oCGs:["OCG_Expired"]});\n  }\n }catch(e){}\n}catch(e){}\n})();`,
 		start.Format(time.RFC3339),
 		end.Format(time.RFC3339),
 	)
